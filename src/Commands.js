e6a38100c62d3d0c976c97022f5f7887f7d53a67
Chris Erin and Vidal Ekechukwu <dev+chriserin+vekh@hashrocket.com>
2017-10-27 16:30:34 -0500

    Betting form styling

--- src/Commands.js
+++ src/Commands.js
@@ -1,47 +1,67 @@
-import React, { Component } from 'react';
-import glam from 'glamorous';
+import React, { Component } from "react";
+import glam from "glamorous";
 
-const Label = glam.label({
-  padding: '4px',
-  display: 'block',
-  width: '300px',
+const FieldSet = glam.div({
+  border: "none",
+  boxSizing: "border-box",
+  display: "flex",
+  flexWrap: "wrap",
+  padding: "1rem",
+  width: "100%"
 });
 
-const LabelSpan = glam.span({
-  width: '110px',
-  display: 'inline-block'
+const Label = glam.label({
+  boxSizing: "border-box",
+  display: "block",
+  padding: "4px",
+  whiteSpace: "nowrap",
+  minWidth: "50%"
+});
+
+const LabelSpan = glam.strong({
+  display: "inline-block",
+  width: "110px"
 });
 
 const Input = glam.input({
-  marginLeft: '8px',
+  border: "solid 0.1rem #CCC",
+  borderRadius: "0.2rem",
+  boxShadow: "none",
+  minWidth: "10rem",
+  padding: "0.7rem 1rem"
 });
 
-const FieldSet = glam.fieldset({
-  width: '300px',
-  float: 'left',
-});
-
-const Number = ({label, type, number, min, max, value, onChange, step}) => (
-  <Label>
-    <LabelSpan>{ label }</LabelSpan>
-    <Input type="number" type={type} number={number} min={min} max={max} value={value} onChange={onChange} step={ step || 1 } />
-  </Label>
-)
-
-const Rotate = ({label, status, onRotate}) => (
+const Number = ({ label, type, number, min, max, value, onChange, step }) => (
   <Label>
     <LabelSpan>{label}</LabelSpan>
-    <a href='#' onClick={onRotate} >{status}</a>
+    <Input
+      type="number"
+      type={type}
+      number={number}
+      min={min}
+      max={max}
+      value={value}
+      onChange={onChange}
+      step={step || 1}
+    />
   </Label>
-)
+);
+
+const Rotate = ({ label, status, onRotate }) => (
+  <Label>
+    <LabelSpan>{label}</LabelSpan>
+    <a href="#" onClick={onRotate}>
+      {status}
+    </a>
+  </Label>
+);
 
 class Commands extends Component {
-
   render() {
     const Container = glam.div({
-      overflow: 'auto',
-      width: '100%',
-      textAlign: 'left'
+      overflow: "auto",
+      width: "100%",
+      textAlign: "left"
     });
 
     let rotateStatus = null;
@@ -53,13 +73,37 @@ class Commands extends Component {
 
     return (
       <Container>
-        <FieldSet>
-          <Number label="Sides" type='number' min='3' max='8' value={this.props.points} onChange={this.props.changePoints }/>
-          <Rotate label="Rotate" status={rotateStatus} onRotate={this.props.onRotate} />
-          <Number label="Hue" step="25" type='number' min='0' max='255' value={this.props.hue} onChange={this.props.changeHue }/>
-        </FieldSet>
-        <FieldSet>
-          <Number label="Layers" type='number' min='2' max='8' value={this.props.layers} onChange={this.props.changeLayers }/>
+        <FieldSet className="fieldset">
+          <Number
+            label="Sides"
+            type="number"
+            min="3"
+            max="8"
+            value={this.props.points}
+            onChange={this.props.changePoints}
+          />
+          <Rotate
+            label="Rotate"
+            status={rotateStatus}
+            onRotate={this.props.onRotate}
+          />
+          <Number
+            label="Hue"
+            step="25"
+            type="number"
+            min="0"
+            max="255"
+            value={this.props.hue}
+            onChange={this.props.changeHue}
+          />
+          <Number
+            label="Layers"
+            type="number"
+            min="2"
+            max="8"
+            value={this.props.layers}
+            onChange={this.props.changeLayers}
+          />
         </FieldSet>
       </Container>
     );
--- src/SquaresContainer.js
+++ src/SquaresContainer.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import Square from 'Square.js';
-import Commands from 'Commands.js';
-import glamorous from 'glamorous';
+import React, { Component } from "react";
+import Square from "Square.js";
+import Commands from "Commands.js";
+import glamorous from "glamorous";
 
 const ContainerDiv = glamorous.div({
-  width: '110%',
-  background: 'LInear-gradient(green, yellow)',
-  height: '100%',
-  position: 'absolute',
-  overflow: 'hidden'
+  width: "110%",
+  background: "LInear-gradient(green, yellow)",
+  height: "100%",
+  position: "absolute",
+  overflow: "hidden"
 });
 
 class SquaresContainer extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.changePoints = this.changePoints.bind(this);
@@ -25,52 +24,54 @@ class SquaresContainer extends Component {
       points: 4,
       layers: 4,
       hue: 25,
-      isRotated: true,
+      isRotated: true
     };
   }
 
   changePoints(event) {
-    this.setState({points: event.target.value});
+    this.setState({ points: event.target.value });
   }
 
   changeLayers(event) {
-    this.setState({layers: event.target.value});
+    this.setState({ layers: event.target.value });
   }
 
   changeHue(event) {
-    this.setState({hue: event.target.value});
+    this.setState({ hue: event.target.value });
   }
 
   onRotate() {
-    console.log('changing rotation state', this.state.isRotated);
-    this.setState((prevState) => ({isRotated: !prevState.isRotated}));
+    console.log("changing rotation state", this.state.isRotated);
+    this.setState(prevState => ({ isRotated: !prevState.isRotated }));
   }
 
   render() {
-    const squares = Array(400).fill(0).map((_, i) => (
-      <Square key={i}
-        hue={this.state.hue}
-        index={i}
-        points={this.state.points}
-        layers={this.state.layers}
-        isRotated={this.state.isRotated}
-      />));
+    const squares = Array(400)
+      .fill(0)
+      .map((_, i) => (
+        <Square
+          key={i}
+          hue={this.state.hue}
+          index={i}
+          points={this.state.points}
+          layers={this.state.layers}
+          isRotated={this.state.isRotated}
+        />
+      ));
 
     return (
       <div>
         <Commands
-          points={ this.state.points }
-          layers={ this.state.layers }
-          hue={ this.state.hue }
-          isRotated={ this.state.isRotated }
-          changeHue={ this.changeHue }
-          changePoints={ this.changePoints }
-          changeLayers={ this.changeLayers }
-          onRotate={ this.onRotate }
+          points={this.state.points}
+          layers={this.state.layers}
+          hue={this.state.hue}
+          isRotated={this.state.isRotated}
+          changeHue={this.changeHue}
+          changePoints={this.changePoints}
+          changeLayers={this.changeLayers}
+          onRotate={this.onRotate}
         />
-        <ContainerDiv>
-        { squares }
-        </ContainerDiv>
+        <ContainerDiv>{squares}</ContainerDiv>
       </div>
     );
   }
